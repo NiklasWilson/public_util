e3dc3f0b2557249ff3ba2b5f2a9e8ddceaefd952
Niklas Wilson <it@niklaswilson.net>
2024-10-05 09:39:56 -0400

    Update vm_gpu_init.sh
    apt changes

--- vm_gpu_init.sh
+++ vm_gpu_init.sh
@@ -7,12 +7,12 @@ apt -y upgrade
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
 dpkg -i cuda-keyring_1.0-1_all.deb
-apt-get update
-apt-get -y install cuda-drivers
+apt update
+apt -y install cuda-drivers
 
 # Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl gnupg
+apt update
+apt install -y ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
@@ -22,8 +22,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt -y update
+apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Install Nvidia Container toolkit
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
@@ -31,15 +31,15 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-apt-get update
-apt-get install -y nvidia-container-toolkit cuda-toolkit
+apt -y update
+apt -y install nvidia-container-toolkit cuda-toolkit
 nvidia-ctk runtime configure --runtime=docker
 systemctl restart docker
 
-apt-get update; apt-get install npm; npm install pm2 -g; pm2 install pm2-logrotate; pm2 set pm2-logrotate:compress true
-pip install nvitop
-apt install ufw; ufw allow 30333/tcp; ufw allow 22/tcp; ufw allow 8000:8999/tcp; ufw allow 8000:8999/udp; ufw enable
+apt update; apt -y install npm; npm install pm2 -g; pm2 install pm2-logrotate; pm2 set pm2-logrotate:compress true
+pip install -y nvitop
+apt install -y ufw; ufw allow 30333/tcp; ufw allow 22/tcp; ufw allow 8000:8999/tcp; ufw allow 8000:8999/udp; ufw enable
 
-#subtensor
-apt update && apt upgrade && apt install --assume-yes build-essential git clang curl libssl-dev llvm libudev-dev make protobuf-compiler && apt autoremove
+# Default Packages
+apt update && apt -y upgrade && apt -y install build-essential git clang curl libssl-dev llvm libudev-dev make protobuf-compiler && apt -y autoremove
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
